4d1d10e9406e6d8f55ee9e236ef415fbd6edcbf0
Naren Bagavathy <infonaren55@gmail.com>
2023-10-09 12:34:42 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HolaGalaxy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola Galaxy!");
+        }
+    }
+}
